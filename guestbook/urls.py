4614b5a68fd09df6806b2f3875096886c8740e49
zhibek17 <primovajibek@gmail.com>
2023-12-09 18:22:44 +0600

    Добавила форму добавления на главную страницу

--- guestbook/urls.py
+++ guestbook/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
-    path('add/', guest_add, name='guest_add')
+    path('update/<int:pk>/', guest_update, name='guest_update'),
+    path('delete/<int:pk>/', guest_delete, name='guest_delete'),
 ]
--- guestbook/views.py
+++ guestbook/views.py
@@ -1,15 +1,10 @@
-from django.http import HttpResponseRedirect
-from django.shortcuts import render, reverse, redirect
+from django.shortcuts import render, reverse, redirect, get_object_or_404
 from .models import GuestBook
 from .forms import GuestForm
 
 
 def index(request):
     guests = GuestBook.objects.filter(status='active').order_by('-created_at')
-    return render(request, 'index.html', {'guests': guests})
-
-
-def guest_add(request):
     if request.method == 'POST':
         form = GuestForm(request.POST)
         if form.is_valid():
@@ -17,4 +12,28 @@ def guest_add(request):
             return redirect('index')
     else:
         form = GuestForm()
-    return render(request, 'guest_add.html', {'form': form})
+    return render(request, 'index.html', {'guests': guests, 'form': form})
+
+
+
+
+
+def guest_update(request, pk):
+    guest = get_object_or_404(GuestBook, pk=pk)
+    if request.method == 'POST':
+        form = GuestForm(request.POST, instance=guest)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = GuestForm(instance=guest)
+    return render(request, 'guest_update.html', {'form': form, 'guest': guest})
+
+
+def guest_delete(request, pk):
+    guest = get_object_or_404(GuestBook, pk=pk)
+    if request.method == 'GET':
+        return render(request, 'guest_delete.html', {'guest': guest})
+    elif request.method == 'POST':
+        guest.delete()
+        return redirect('index')
