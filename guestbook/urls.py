fe018fc98b13bfa338cee584543b26bb7f9bf99f
zhibek17 <primovajibek@gmail.com>
2023-12-09 18:49:55 +0600

    Добавила ссылки на страницы

--- guestbook/urls.py
+++ guestbook/urls.py
@@ -3,6 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
+    path('add/', guest_add, name='guest_add'),
     path('update/<int:pk>/', guest_update, name='guest_update'),
     path('delete/<int:pk>/', guest_delete, name='guest_delete'),
 ]
