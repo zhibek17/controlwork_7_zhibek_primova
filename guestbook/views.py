047c65a78228f1ff151008bd452c95df67edcd29
zhibek17 <primovajibek@gmail.com>
2023-12-09 18:48:08 +0600

    Добавила функцию удаления, обновления и добавления новой записи

--- guestbook/views.py
+++ guestbook/views.py
@@ -1,21 +1,23 @@
 from django.shortcuts import render, reverse, redirect, get_object_or_404
 from .models import GuestBook
 from .forms import GuestForm
+from django.http import HttpResponseRedirect
 
 
 def index(request):
     guests = GuestBook.objects.filter(status='active').order_by('-created_at')
+    return render(request, 'index.html', {'guests': guests})
+
+
+def guest_add(request):
     if request.method == 'POST':
         form = GuestForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('index')
+            return HttpResponseRedirect(reverse('index'))
     else:
         form = GuestForm()
-    return render(request, 'index.html', {'guests': guests, 'form': form})
-
-
-
+    return render(request, 'guest_add.html', {'form': form})
 
 
 def guest_update(request, pk):
